690f9ca92284e8951ddcdf185a71e1de7bed9927
GitHub Action <action@github.com>
2022-10-06 15:20:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/faithdeveloper/testplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/faithdeveloper/testplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.faithdeveloper.testplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.faithdeveloper.testplugintemplate.MyBundle
 
 class MyApplicationService {
 
