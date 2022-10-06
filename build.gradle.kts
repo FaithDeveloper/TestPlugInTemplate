794f442f8dbee946b6b611792123ade6003ba243
KYU <dev.faithpeople@gmail.com>
2022-10-07 00:24:49 +0900

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ intellij {
     pluginName.set(properties("pluginName"))
     version.set(properties("platformVersion"))
     type.set(properties("platformType"))
+    
+    intellij.localPath.set(properties("StudioRunPath"))
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
